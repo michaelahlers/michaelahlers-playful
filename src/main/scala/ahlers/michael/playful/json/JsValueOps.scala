60327c0409d0b2a1719a3438642d2426a98aceda
Michael Ahlers <michael@ahlers.co>
2016-02-26 18:08:00 -0500

    Add documentation

--- src/main/scala/ahlers/michael/playful/json/JsValueOps.scala
+++ src/main/scala/ahlers/michael/playful/json/JsValueOps.scala
@@ -6,6 +6,36 @@ import scala.annotation.tailrec
 
 class JsValueOps(value: JsValue) {
 
+  /**
+   * Produces a [[List]] containing materialized paths to all leaf values (*i.e.*, not [[JsObject]]). It will accept a leaf value (including [[JsArray]]), positioning it on a root path. No special processing is done to preserve order of object keys, however correct array indices are guaranteed.
+   *
+   * To illustrate:
+   * {{{
+val expected =
+  Set(
+    (__ \ 'foo, JsString("bear")),
+    (__ \ 'integers apply 0, JsNumber(10)),
+    (__ \ 'integers apply 1, JsNumber(11)),
+    (__ \ 'fiz \ 'ban \ 'zif, JsString("nab")),
+    (__ \ 'fiz \ 'ban \ 'integers apply 0, JsNumber(100)),
+    (__ \ 'fiz \ 'ban \ 'integers apply 1, JsNumber(101))
+  )
+
+val actual =
+  materialized(obj(
+    "foo" -> "bear",
+    "fiz" -> obj(
+      "ban" -> obj(
+        "zif" -> "nab",
+        "integers" -> arr(100, 101)
+      )
+    ),
+    "integers" -> arr(10, 11)
+  ))
+
+actual.toSet == expected
+   * }}}
+   */
   def materialized: List[(JsPath, JsValue)] = {
 
     @tailrec
