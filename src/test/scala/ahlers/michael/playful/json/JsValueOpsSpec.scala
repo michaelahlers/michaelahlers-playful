82a31de7519254291e4dfaa91d72cc57ae62d305
Michael Ahlers <michael@ahlers.co>
2016-02-27 11:26:54 -0500

    Add tests

--- src/test/scala/ahlers/michael/playful/json/JsValueOpsSpec.scala
+++ src/test/scala/ahlers/michael/playful/json/JsValueOpsSpec.scala
@@ -2,7 +2,7 @@ package ahlers.michael.playful.json
 
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{Matchers, WordSpec}
-import play.api.libs.json.Json.{arr, toJson}
+import play.api.libs.json.Json.{arr, obj, toJson}
 import play.api.libs.json._
 
 class JsValueOpsSpec
@@ -34,5 +34,29 @@ class JsValueOpsSpec
     }
   }
 
+  "Shallow objects" must {
+    "materialize" in {
+      val expected =
+        __ \ 'null -> JsNull ::
+          __ \ 'boolean -> toJson(true) ::
+          __ \ 'number -> toJson(10) ::
+          __ \ 'string -> toJson("") ::
+          (__ \ 'array apply 0, toJson(10)) ::
+          (__ \ 'array apply 1, toJson(11)) ::
+          (__ \ 'array apply 2, toJson(12)) ::
+          Nil
+
+      val actual =
+        JsValues.materialized(obj(
+          "null" -> JsNull,
+          "boolean" -> true,
+          "number" -> 10,
+          "string" -> "",
+          "array" -> List(10, 11, 12)
+        ))
+
+      actual should contain theSameElementsAs expected
+    }
+  }
 
 }
