a62375ec8e5e22876e548dac1f14ae65c7bf629e
Michael Ahlers <michael@ahlers.co>
2016-02-28 11:44:02 -0500

    Test full functionality

--- src/test/scala/ahlers/michael/playful/json/TraversableJsResultOpsSpec.scala
+++ src/test/scala/ahlers/michael/playful/json/TraversableJsResultOpsSpec.scala
@@ -9,17 +9,23 @@ class TraversableJsResultOpsSpec
           with Matchers
           with LazyLogging {
 
-  "Marshaled result collections" must {
-    "preserve errors" in {
-      val successes: Seq[JsResult[Int]] = (0 until 2).map(JsSuccess(_))
-      val errors: Seq[JsResult[Int]] = (0 until 2).map({ i => JsError(__ \ 'path \ i.toString, s"$i error") })
+  "Result collections" must {
 
+    val successes: Seq[JsResult[Int]] = (0 until 2).map(JsSuccess(_))
+    val errors: Seq[JsResult[Int]] = (0 until 2).map({ i => JsError(__ \ 'path \ i.toString, s"$i error") })
 
+    "marshal any errors" in {
       val expected = errors.collect({ case e: JsError => e }).reduce(JsError.merge(_, _))
       val actual = TraversableJsResults.marshaled(successes ++ errors)
-
       actual should be(expected)
     }
+
+    "marshal success" in {
+      val expected: JsSuccess[Seq[Int]] = JsSuccess(successes.collect({ case JsSuccess(v, _) => v }))
+      val actual = TraversableJsResults.marshaled(successes)
+      actual should be(expected)
+    }
+
   }
 
 }
