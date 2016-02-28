f127f9dfac280811fb7b052d533b0569e28b8590
Michael Ahlers <michael@ahlers.co>
2016-02-28 12:00:46 -0500

    Implement and pass tests

--- src/main/scala/ahlers/michael/playful/json/TraversableJsResultOps.scala
+++ src/main/scala/ahlers/michael/playful/json/TraversableJsResultOps.scala
@@ -1,16 +1,32 @@
 package ahlers.michael.playful.json
 
-import play.api.libs.json.{JsResult, JsSuccess}
+import play.api.libs.json.{JsError, JsResult, JsSuccess}
 
 import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable.Builder
 import scala.language.higherKinds
 
 class TraversableJsResultOps[E, C[X] <: Traversable[X]](results: C[JsResult[E]]) {
 
   def marshaled(implicit cbf: CanBuildFrom[C[JsResult[E]], E, C[E]]): JsResult[C[E]] = {
-    val builder = cbf(results)
+    val builder: Builder[E, C[E]] = cbf(results)
     builder.sizeHint(results)
-    JsSuccess(builder.result())
+
+    results.foldLeft[JsResult[Builder[E, C[E]]]](JsSuccess(builder)) {
+
+      case (JsSuccess(a, _), JsSuccess(e, _)) =>
+        JsSuccess(a += e)
+
+      case (JsSuccess(a, _), e: JsError) =>
+        e
+
+      case (a: JsError, JsSuccess(e, _)) =>
+        a
+
+      case (a: JsError, e: JsError) =>
+        JsError.merge(a, e)
+
+    } map (_.result())
   }
 
 }
--- src/test/scala/ahlers/michael/playful/json/TraversableJsResultOpsSpec.scala
+++ src/test/scala/ahlers/michael/playful/json/TraversableJsResultOpsSpec.scala
@@ -14,13 +14,19 @@ class TraversableJsResultOpsSpec
     val successes: Seq[JsResult[Int]] = (0 until 2).map(JsSuccess(_))
     val errors: Seq[JsResult[Int]] = (0 until 2).map({ i => JsError(__ \ 'path \ i.toString, s"$i error") })
 
-    "marshal any errors" in {
-      val expected = errors.collect({ case e: JsError => e }).reduce(JsError.merge(_, _))
+    "marshal all errors" in {
+      val expected = errors.collect({ case e: JsError => e }).reduce[JsError](JsError.merge)
+      val actual = TraversableJsResults.marshaled(errors)
+      actual should be(expected)
+    }
+
+    "marshal mixed results" in {
+      val expected = errors.collect({ case e: JsError => e }).reduce[JsError](JsError.merge)
       val actual = TraversableJsResults.marshaled(successes ++ errors)
       actual should be(expected)
     }
 
-    "marshal success" in {
+    "marshal all successes" in {
       val expected: JsSuccess[Seq[Int]] = JsSuccess(successes.collect({ case JsSuccess(v, _) => v }))
       val actual = TraversableJsResults.marshaled(successes)
       actual should be(expected)
