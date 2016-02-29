2141d7856c2b4d33a0663b709f9da743a56b6086
Michael Ahlers <michael@ahlers.co>
2016-02-28 19:40:53 -0500

    Use a mutable buffer to accumulate errors (for #4)

--- src/main/scala/ahlers/michael/playful/json/TraversableJsResultOps.scala
+++ src/main/scala/ahlers/michael/playful/json/TraversableJsResultOps.scala
@@ -1,37 +1,42 @@
 package ahlers.michael.playful.json
 
-import play.api.libs.json.{JsError, JsResult, JsSuccess}
+import play.api.data.validation.ValidationError
+import play.api.libs.json.{JsError, JsPath, JsResult, JsSuccess}
 
 import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable
 import scala.collection.mutable.Builder
 import scala.language.higherKinds
 
 class TraversableJsResultOps[E, C[X] <: Traversable[X]](results: C[JsResult[E]]) {
 
   def marshaled(implicit cbf: CanBuildFrom[C[JsResult[E]], E, C[E]]): JsResult[C[E]] = {
+
+    type Error = (JsPath, Seq[ValidationError])
+
     val values: Builder[E, C[E]] = cbf(results)
     values.sizeHint(results)
 
     val result =
-      results.foldLeft[JsResult[Builder[E, C[E]]]](JsSuccess(values)) {
+      results.foldLeft[Either[Builder[E, C[E]], mutable.Buffer[Error]]](Left(values)) {
 
-        case (JsSuccess(a, _), JsSuccess(e, _)) =>
-          JsSuccess(a += e)
+        case (Left(a), JsSuccess(e, _)) =>
+          Left(a += e)
 
-        case (JsSuccess(a, _), e: JsError) =>
-          e
+        case (Left(_), e: JsError) =>
+          Right(mutable.Buffer.empty ++ e.errors)
 
-        case (a: JsError, JsSuccess(e, _)) =>
-          a
+        case (Right(a), JsSuccess(_, _)) =>
+          Right(a)
 
-        case (a: JsError, e: JsError) =>
-          JsError(a.errors ++ e.errors)
+        case (Right(a), e: JsError) =>
+          Right(a ++= e.errors)
 
       }
 
     result match {
-      case JsSuccess(v, p) => JsSuccess(v.result(), p)
-      case e: JsError => e ++ JsError(Nil)
+      case Left(a) => JsSuccess(a.result())
+      case Right(a) => JsError(a.toSeq) ++ JsError(Nil)
     }
 
   }
