e637af2490f845e982d9724a70eec343ef0be2a1
Michael Ahlers <michael@ahlers.co>
2016-02-29 20:56:23 -0500

    Benchmark JsValue materialization

---
+++ src/bench/scala/ahlers/michael/playful/json/JsValueOpsBenchmark.scala
@@ -0,0 +1,33 @@
+package ahlers.michael.playful.json
+
+import org.scalameter._
+import play.api.libs.json._
+
+object JsValueOpsBenchmark extends Bench.OfflineReport {
+
+  val sizes = Gen.range("results")(100, 1000, 100)
+
+  val flat: Gen[JsValue] = for {
+    size <- sizes
+  } yield JsObject((0 until size).map({ i => s"path$i" -> JsString(s"value$i") }))
+
+  val deep: Gen[JsValue] = for {
+    size <- sizes
+  } yield (size until 0).foldLeft[JsValue](JsString("value")) { case (a, i) => JsObject(List(s"path$i" -> a)) }
+
+  performance of "Value operations" in {
+
+    measure method "materialize flat objects" in {
+      using(flat) in { value =>
+        JsValues.materialized(value)
+      }
+    }
+
+    measure method "materialize deep objects" in {
+      using(deep) in { value =>
+        JsValues.materialized(value)
+      }
+    }
+
+  }
+}
