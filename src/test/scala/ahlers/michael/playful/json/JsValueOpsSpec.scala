b60dfb0a989cb853eeafd19c9bd8040a6ae378c4
Michael Ahlers <michael@ahlers.co>
2016-03-01 20:55:14 -0500

    Make room for additional functions

--- src/test/scala/ahlers/michael/playful/json/JsValueOpsSpec.scala
+++ src/test/scala/ahlers/michael/playful/json/JsValueOpsSpec.scala
@@ -10,78 +10,100 @@ class JsValueOpsSpec
           with Matchers
           with LazyLogging {
 
-  "Non-object values" must {
-    "materialize" when {
+  "Materialized" when {
+
+    "passed non-objects" must {
       val exemplars =
         JsNull ::
           toJson(true) ::
           toJson(10) ::
           toJson("string") ::
-          arr(1, 2, 3) ::
           Nil
 
       exemplars foreach { exemplar =>
-        s"given $exemplar" in {
-          val expected =
-            exemplar match {
-              case JsArray(elements) => elements.zipWithIndex.map({ case (e, i) => JsPath(i) -> e })
-              case _ => List(__ -> exemplar)
-            }
-
+        s"put $exemplar at empty paths" in {
+          val expected = List(__ -> exemplar)
           JsValues.materialized(exemplar) should contain theSameElementsAs expected
         }
       }
-    }
-  }
 
-  "Objects" must {
-    "materialize" in {
-      val expected =
-        __ \ 'foo \ 'null -> JsNull ::
-          __ \ 'foo \ 'boolean -> toJson(true) ::
-          __ \ 'bear \ 'number -> toJson(10) ::
-          __ \ 'bear \ 'string -> toJson("") ::
+    }
+
+    "passed arrays of non-objects" must {
+
+      "put values at indexed paths" in {
+        val exemplar = List(1, 2, 3)
+        val expected = exemplar.zipWithIndex.map({ case (v, i) => JsPath.apply(i) -> toJson(v) })
+        JsValues.materialized(toJson(exemplar)) should contain theSameElementsAs expected
+      }
+
+    }
+
+    "passed objects" must {
+
+      "put leaf-values on key paths" in {
+        val expected =
+          __ \ 'foo \ 'null -> JsNull ::
+            __ \ 'foo \ 'boolean -> toJson(true) ::
+            __ \ 'bear \ 'number -> toJson(10) ::
+            __ \ 'bear \ 'string -> toJson("") ::
+            Nil
+
+        val actual =
+          JsValues.materialized(obj(
+            "foo" -> obj(
+              "null" -> JsNull,
+              "boolean" -> true
+            ),
+            "bear" -> obj(
+              "number" -> 10,
+              "string" -> ""
+            )
+          ))
+
+        actual should contain theSameElementsAs expected
+      }
+
+      "put array values on indexed paths" in {
+        val expected =
           (__ \ 'foobear \ 'array apply 0, toJson(10)) ::
-          (__ \ 'foobear \ 'array apply 1, toJson(11)) ::
-          (__ \ 'foobear \ 'array apply 2, toJson(12)) ::
-          Nil
+            (__ \ 'foobear \ 'array apply 1, toJson(11)) ::
+            (__ \ 'foobear \ 'array apply 2, toJson(12)) ::
+            Nil
 
-      val actual =
-        JsValues.materialized(obj(
-          "foo" -> obj(
-            "null" -> JsNull,
-            "boolean" -> true
-          ),
-          "bear" -> obj(
-            "number" -> 10,
-            "string" -> ""
-          ),
-          "foobear" -> obj(
-            "array" -> List(10, 11, 12)
-          )
-        ))
+        val actual =
+          JsValues.materialized(obj(
+            "foobear" -> obj(
+              "array" -> List(10, 11, 12)
+            )
+          ))
 
-      actual should contain theSameElementsAs expected
+        actual should contain theSameElementsAs expected
+      }
+
     }
-  }
 
-  "Arrays of objects" must {
-    "materialize" in {
-      val expected =
-        (JsPath(0) \ 'foo, toJson("bear")) ::
-          (JsPath(0) \ 'fiz, toJson("ban")) ::
-          (JsPath(1) \ 'fu, toJson("bar")) ::
-          (JsPath(1) \ 'zif, toJson("nab")) ::
-          Nil
+    "passed arrays of objects" must {
 
-      val actual =
-        JsValues.materialized(arr(
-          obj("foo" -> "bear", "fiz" -> "ban"),
-          obj("fu" -> "bar", "zif" -> "nab")
-        ))
+      "put leaf-values on keyed paths at indexes" in {
+        val expected =
+          (JsPath(0) \ 'foo, toJson("bear")) ::
+            (JsPath(0) \ 'fiz, toJson("ban")) ::
+            (JsPath(1) \ 'fu, toJson("bar")) ::
+            (JsPath(1) \ 'zif, toJson("nab")) ::
+            Nil
 
-      actual should contain theSameElementsAs expected
+        val actual =
+          JsValues.materialized(arr(
+            obj("foo" -> "bear", "fiz" -> "ban"),
+            obj("fu" -> "bar", "zif" -> "nab")
+          ))
+
+        actual should contain theSameElementsAs expected
+      }
+
     }
+
   }
 
 }
