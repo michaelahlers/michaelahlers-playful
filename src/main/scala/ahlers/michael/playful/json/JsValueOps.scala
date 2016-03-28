ee39cc999d1bb7f3c7303fe1856620163beaba03
Michael Ahlers <michael@ahlers.co>
2016-03-28 16:09:18 -0400

    Rename and fix bad sample block formatting

--- src/main/scala/ahlers/michael/playful/json/JsValueOps.scala
+++ src/main/scala/ahlers/michael/playful/json/JsValueOps.scala
@@ -7,40 +7,40 @@ import play.api.libs.json._
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 
-class JsValueOps(value: JsValue) {
+class JsValueOps(subject: JsValue) {
 
   /**
    * Produces a [[scala.List]] containing materialized paths to all leaf values (''i.e.'', not [[play.api.libs.json.JsObject]]). It will accept a leaf value (including [[play.api.libs.json.JsArray]]), positioning it on a root path. No special processing is done to preserve order of object keys, however correct array indices are guaranteed.
    *
    * To illustrate:
    * {{{
-   * import ahlers.michael.playful.json.JsValues.materialized
-   * import play.api.libs.json.Json._
-   * import play.api.libs.json._
-   * *
-  val expected =
-   * Set(
-   * (__ \ 'foo, JsString("bear")),
-   * (__ \ 'integers apply 0, JsNumber(10)),
-   * (__ \ 'integers apply 1, JsNumber(11)),
-   * (__ \ 'fiz \ 'ban \ 'zif, JsString("nab")),
-   * (__ \ 'fiz \ 'ban \ 'integers apply 0, JsNumber(100)),
-   * (__ \ 'fiz \ 'ban \ 'integers apply 1, JsNumber(101))
-   * )
-   * *
-  val actual =
-   * materialized(obj(
-   * "foo" -> "bear",
-   * "fiz" -> obj(
-   * "ban" -> obj(
-   * "zif" -> "nab",
-   * "integers" -> arr(100, 101)
-   * )
-   * ),
-   * "integers" -> arr(10, 11)
-   * ))
-   * *
-  assert(actual.toSet == expected)
+import ahlers.michael.playful.json.JsValues.materialized
+import play.api.libs.json.Json._
+import play.api.libs.json._
+
+val expected =
+  Set(
+    (__ \ 'foo, JsString("bear")),
+    (__ \ 'integers apply 0, JsNumber(10)),
+    (__ \ 'integers apply 1, JsNumber(11)),
+    (__ \ 'fiz \ 'ban \ 'zif, JsString("nab")),
+    (__ \ 'fiz \ 'ban \ 'integers apply 0, JsNumber(100)),
+    (__ \ 'fiz \ 'ban \ 'integers apply 1, JsNumber(101))
+  )
+
+val actual =
+  materialized(obj(
+    "foo" -> "bear",
+    "fiz" -> obj(
+      "ban" -> obj(
+        "zif" -> "nab",
+        "integers" -> arr(100, 101)
+      )
+    ),
+    "integers" -> arr(10, 11)
+  ))
+
+assert(actual.toSet == expected)
    * }}}
    */
   def materialized: List[(JsPath, JsValue)] = {
@@ -65,11 +65,11 @@ class JsValueOps(value: JsValue) {
 
       }
 
-    traverse(List(Queue.empty -> value), Nil)
+    traverse(List(Queue.empty -> subject), Nil)
 
   }
 
-  def updated[T](path: JsPath, assignment: T)(implicit w: Writes[T]): JsValue = {
+  def updated[T](path: JsPath, value: T)(implicit w: Writes[T]): JsValue = {
 
     def JsObjectLens(field: String, default: => JsValue) = Lens[JsValue, JsValue](_ \ field getOrElse default) {
       assignment => {
@@ -122,7 +122,7 @@ class JsValueOps(value: JsValue) {
 
       }
 
-    lens(path.path, Lens.id) set w.writes(assignment) apply value
+    lens(path.path, Lens.id) set w.writes(value) apply subject
 
   }
 
